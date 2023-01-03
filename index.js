d2991ff44556f64fd003773ef810b6c1f4cfc620
GLEADS0\huynguyen <huy.nguyen@gleads.vn>
2023-01-03 14:38:40 +0700

    created

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const pathCurrent = process.cwd();
+
+const fs = require("fs");
+const templates = {
+  react: {
+    input: `./templates/Packages.js`,
+    output: `${pathCurrent}/Packages.js`,
+  },
+};
+
+renderFile = (input, output) => {
+  const content = fs.readFileSync(input);
+  fs.writeFileSync(output, content);
+};
+
+writeTemplate = () => {
+  const argvs = process.argv;
+  console.log(argvs[2]);
+  console.log(templates);
+
+  try {
+    renderFile(templates[argvs[2]]["input"], templates[argvs[2]]["output"]);
+    console.log("Component Packages create successfully");
+  } catch (e) {
+    console.log("Nền tảng chưa được hỗ trợ");
+  }
+};
+
+writeTemplate();
